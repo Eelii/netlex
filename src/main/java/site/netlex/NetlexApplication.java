58e2a1ff93ad65b4122e17e491fa62e46bd369f5
Eelii <61108072+Eelii@users.noreply.github.com>
2021-03-16 14:44:08 +0200

    first!

---
+++ src/main/java/site/netlex/NetlexApplication.java
@@ -0,0 +1,13 @@
+package site.netlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetlexApplication.class, args);
+	}
+
+}
