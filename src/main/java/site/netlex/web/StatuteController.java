c60d586dad13ae92ee9fb86a6c0897f6f693f49c
Eelii <61108072+Eelii@users.noreply.github.com>
2021-03-24 19:17:10 +0200

    Helloooo

--- src/main/java/site/netlex/web/StatuteController.java
+++ src/main/java/site/netlex/web/StatuteController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import site.netlex.domain.Section;
 import site.netlex.domain.SectionForm;
@@ -56,29 +58,46 @@ public class StatuteController {
 		    			bindingResult.rejectValue("statuteId", "err.statuteId", "Säädös on jo tietokannassa");    	
 		    			return "uusisaados";		    		
 			    	}
-	    	return "redirect:/muokkaa/" + newStatute.getStatDbId();
+	    	return "redirect:/muokkaamain/" + newStatute.getStatDbId();
 	    }    
 	
+	
+	@RequestMapping(value = "/statDbId", method = RequestMethod.POST)
+	public String postStatDbId(@Valid @ModelAttribute("statDbId") Long statDbId) {
+		
+		return "redirect:/uusipykala/" + String.valueOf(statDbId);
+	}
+	
+	@RequestMapping(value = "/uusipykala/{statdbid}", method = RequestMethod.GET)
+	public String createSection(@Valid @ModelAttribute("sectionform") SectionForm sectionForm, @PathVariable("statdbid") Long statDbId, Model model){
+		model.addAttribute("sectionform", new SectionForm());
+		Optional<Statute> op = statRepository.findById(statDbId);
+		model.addAttribute("statDbId", op.get().getStatDbId());
+		model.addAttribute("statute", op.get());
+		
+		return "uusipykala";
+	}
 	
 	
 	@RequestMapping(value = "/savesection", method = RequestMethod.POST)
     public String save(@Valid @ModelAttribute("sectionform") SectionForm sectionForm, BindingResult bindingResult) 	{	
     			
 		Section newSection = new Section();
-		Statute stat = statRepository.findByStatDbId(sectionForm.getStatuteDbId());
+		String statDbId = sectionForm.getStatuteDbId();
+		Statute stat = statRepository.findByStatDbId(statDbId);
 		newSection.setStatute(stat);
 		newSection.setIdentifier(sectionForm.getIdentifier());
 		newSection.setHeading(sectionForm.getHeading());
-    	return null;
+    	return "redirect:/muokkaamain/"+sectionForm.getStatuteDbId();
     }    
 	
 	
-	@RequestMapping(value = "/muokkaa/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/muokkaamain/{id}", method = RequestMethod.GET)
 	public String editStatute(@PathVariable("id") Long statDbId, Model model) {
 		Optional<Statute> op = statRepository.findById(statDbId);
 		model.addAttribute("statute", op.get());
-		model.addAttribute("sectionform", new SectionForm());
-		return "muokkaa";
+		model.addAttribute("statDbId", op.get().getStatDbId());
+		return "muokkaamain";
 	}
 	
 	
